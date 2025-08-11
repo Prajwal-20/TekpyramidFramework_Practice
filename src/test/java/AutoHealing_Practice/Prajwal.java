3d410ef86681a56a3a3a6deba8a44056f735e6aa
USER <USER@LAPTOP-A7S14MCN.mshome.net>
2025-08-11 17:04:07 +0530

    Added prajwal class

---
+++ src/test/java/AutoHealing_Practice/Prajwal.java
@@ -0,0 +1,11 @@
+package AutoHealing_Practice;
+
+public class Prajwal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Fake");
+	}
+
+}
